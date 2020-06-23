95f35df0c6424f46032decdc9f5fede4acefded6
jakub <jakub.kaczmarek2000@gmail.com>
2020-06-23 20:27:32 +0200

    Added getting turn info

--- ai.js
+++ ai.js
@@ -1,10 +1,12 @@
-/* GAME RULES TOGGLING */
 $("#game_end").hide()
 $("#rules").hide()
 $("#player_frame").hide()
 $("#turn_cards").hide()
+$("#turn_check").hide()
+
 var players = 0;
 var bot = 1;
+var number, cards
 
 bot1()
 example()
@@ -49,12 +51,10 @@ function get_players(){
 	return players_list
 }
 
-function confirm_cards(){
-
-}
-
 function start_game(){
 	$("#before_game").hide()
+	$("#game_start").hide()
+	$("#game_end").show()
 	$("#player_frame").show()
 	$("#turn_cards").show()
 	var players_list = get_players()
@@ -66,8 +66,42 @@ function end_game(){
 
 function bot1(){
 	$("body").css({"background-color":"#000033"})
+	bot = 1
 }
 
 function bot2(){
 	$("body").css({"background-color":"#4d0000"})
+	bot = 2
+}
+
+function bot_color(){
+	if (bot == 1)
+		return "#000033"
+	else
+		return "#4d0000"
+}
+
+function number_of_cards(){
+	$(".number_of_cards").css({"background-color":bot_color(),"color":"white"})
+	$(event.target).css({"background-color":"white","color":"black"})
+	number = $(event.target).html()
+	console.log(number)
+}
+
+function card_rank(){
+	$(".card_rank").css({"background-color":bot_color(),"color":"white"})
+	$(event.target).css({"background-color":"white","color":"black"})
+	rank = $(event.target).html()
+	console.log(rank)
+}
+
+function confirm_cards(){
+	$("#turn_cards").hide()
+	$("#turn_check").show()
+
+}
+
+function confirm_check(){
+	$("#turn_check").hide()
+
 }
