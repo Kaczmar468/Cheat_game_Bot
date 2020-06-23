371933466d29549056da5a5117e987644f79b0aa
jakub <jakub.kaczmarek2000@gmail.com>
2020-06-23 17:17:49 +0200

    Implemented adding players

--- ai.js
+++ ai.js
@@ -8,4 +8,31 @@ $("#rules_button").click(function(){
 	$("#rules").toggle();
 });
 
-console.log(5 * 6);
+var players = 0;
+
+function add_player(){
+	var nick = $("#input").val();
+	console.log(nick)
+	$("#input").val("")
+	var before_nick = "<div class='player' id='player"+ players +"'><p class='nick'>";
+	var after_nick = "</p><button class='player_button' onclick='remove_player(player"+players+")'>Remove</button><div class='clear_both'></div></div>";
+	$("#players").append(before_nick+nick+after_nick)
+	players++;
+}
+
+function remove_player(playerid){
+	//console.log(playerid)
+	$(playerid).remove()
+}
+
+function example(){
+	$("#input").val("Roma")
+	add_player()
+	$("#input").val("Kuba")
+	add_player()
+	$("#input").val("Ala")
+	add_player()
+}
+example();
+
+//console.log(5 * 6);
