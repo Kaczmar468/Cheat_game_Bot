4390698f6d16354b11d1d352ed023c755414d35a
jakub <jakub.kaczmarek2000@gmail.com>
2020-06-23 16:05:25 +0200

    updated <head>

--- ai.js
+++ ai.js
@@ -1,6 +1,9 @@
 /* GAME RULES TOGGLING */
 $("#game_end").hide()
 $("#rules").hide()
+$("#player_frame").hide()
+$("#number_frame").hide()
+$("#rank_frame").hide()
 $("#rules_button").click(function(){
 	$("#rules").toggle();
 });
